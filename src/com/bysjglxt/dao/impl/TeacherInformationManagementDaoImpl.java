4eb1c1cf84593b6c4bce35950e64d76b81aac81c
JXX <ystartor@gmail.com>
2017-10-26 20:00:10 +0800

    教师信息管理模块service层以及dao层的实现

--- src/com/bysjglxt/dao/impl/TeacherInformationManagementDaoImpl.java
+++ src/com/bysjglxt/dao/impl/TeacherInformationManagementDaoImpl.java
@@ -1,9 +1,14 @@
 package com.bysjglxt.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import com.bysjglxt.dao.TeacherInformationManagementDao;
+import com.bysjglxt.domain.DO.bysjglxt_teacher_basic;
+import com.bysjglxt.domain.DO.bysjglxt_teacher_user;
 
 public class TeacherInformationManagementDaoImpl implements TeacherInformationManagementDao {
 
@@ -15,5 +20,79 @@ public class TeacherInformationManagementDaoImpl implements TeacherInformationMa
 
 	public Session getSession() {
 		return this.sessionFactory.getCurrentSession();
+	}
+
+	@Override
+	public List<bysjglxt_teacher_user> list_TeacherUserInformation_All() {
+		Session session = getSession();
+		List<bysjglxt_teacher_user> listAllTeacherUserInformation = null;
+		String hql = "from bysjglxt_teacher_user";
+		Query query = session.createQuery(hql);
+		listAllTeacherUserInformation = query.list();
+		return listAllTeacherUserInformation;
+	}
+
+	@Override
+	public bysjglxt_teacher_basic get_TeacherBasicInformation_ByUserBasic(String user_teacher_basic) {
+		Session session = getSession();
+		bysjglxt_teacher_basic TeacherBasicInformation = null;
+		String hql = "from bysjglxt_teacher_basic where user_teacher_basic='" + user_teacher_basic + "'";
+		Query query = session.createQuery(hql);
+		TeacherBasicInformation = (bysjglxt_teacher_basic) query.uniqueResult();
+		return TeacherBasicInformation;
+	}
+
+	@Override
+	public boolean saveTeacherBasic(bysjglxt_teacher_basic bysjglxt_teacher_basic) {
+		boolean flag = false;
+		try {
+			Session session = getSession();
+			session.save(bysjglxt_teacher_basic);
+		} catch (Exception e) {
+			flag = false;
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	@Override
+	public boolean saveTeacher(bysjglxt_teacher_user bysjglxt_teacher_user) {
+		boolean flag = false;
+		try {
+			Session session = getSession();
+			session.save(bysjglxt_teacher_user);
+		} catch (Exception e) {
+			flag = false;
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	@Override
+	public bysjglxt_teacher_user getStudentByNum(String teacher_num) {
+		Session session = getSession();
+		bysjglxt_teacher_user TeacherInformation = null;
+		String hql = "from bysjglxt_teacher_user where user_teacher_num='" + teacher_num + "'";
+		Query query = session.createQuery(hql);
+		TeacherInformation = (bysjglxt_teacher_user) query.uniqueResult();
+		return TeacherInformation;
+	}
+
+	@Override
+	public boolean deleteTeacherBasicInfoById(String user_teacher_basic) {
+		Session session = getSession();
+		String hql = "delete from bysjglxt_teacher_basic where teacher_basic_id='" + user_teacher_basic + "'";
+		Query query = session.createQuery(hql);
+		query.executeUpdate();
+		return true;
+	}
+
+	@Override
+	public boolean deleteTeacherInfoById(String user_teacher_id) {
+		Session session = getSession();
+		String hql = "delete from bysjglxt_teacher_user where user_teacher_id='" + user_teacher_id + "'";
+		Query query = session.createQuery(hql);
+		query.executeUpdate();
+		return true;
 	}
 }
--- src/com/bysjglxt/service/TeacherInformationManagementService.java
+++ src/com/bysjglxt/service/TeacherInformationManagementService.java
@@ -14,8 +14,10 @@ public interface TeacherInformationManagementService {
 	 * @param TeacherExcel
 	 * 
 	 * @return List<bysjglxt_teacher_basic>
+	 * @throws Exception
 	 */
-	public List<bysjglxt_teacher_basic> convertTeacherExcelToList(File teacherExcel);
+	public List<bysjglxt_teacher_basic> convertTeacherExcelToList(File teacherExcel, String EXCEL_TeacherFileName)
+			throws Exception;
 
 	/**
 	 * 将教师基础信息List生成bysjglxt_teacher_basic以及基于此生成的bysjglxt_user_teacher，存储到数据库中
@@ -33,4 +35,23 @@ public interface TeacherInformationManagementService {
 	 * @return
 	 */
 	public List<TeacherInformationDTO> list_TeacherInformationDTO_All();
+
+	/**
+	 * 通过领导小组组长手动添加的教师记录，生成用户表及基础信息表，一并存入数据库中
+	 * 
+	 * @param student_basic
+	 *            手动输入的教师基础信息
+	 * @return 是否存储成功,1是 0否
+	 */
+	public boolean save_NewTeacher(bysjglxt_teacher_basic teacher_basic);
+
+	/**
+	 * 通过多选删除选中的教师用户表以及基础信息表，
+	 * 
+	 * @param useStudentNumList
+	 *            由要删除的教师工号组成的列表
+	 * @return 是否删除成功,1是 0否
+	 */
+	public boolean remove_TeacherList(List<String> useTeacherNumList);
+
 }
