41e6d22f0faffbef335d6d336ac3063b750b45c8
ZB <www.1037976812@qq.com>
2017-10-29 00:17:12 +0800

    更新接口

--- src/com/bysjglxt/service/TeacherInformationManagementService.java
+++ src/com/bysjglxt/service/TeacherInformationManagementService.java
@@ -3,13 +3,17 @@ package com.bysjglxt.service;
 import java.io.File;
 import java.util.List;
 
+import com.bysjglxt.domain.DO.bysjglxt_section;
 import com.bysjglxt.domain.DO.bysjglxt_teacher_basic;
 import com.bysjglxt.domain.DTO.TeacherInformationDTO;
+import com.bysjglxt.domain.VO.TeacherInformationManagementVO;
 
 public interface TeacherInformationManagementService {
 
 	/**
-	 * 教师信息的Excel文件流转化为bysjglxt_Teacher_basic的List
+	 * @待测试
+	 * 
+	 * @说明 教师信息的Excel文件流转化为bysjglxt_Teacher_basic的List
 	 *
 	 * @param TeacherExcel
 	 * @return List<bysjglxt_teacher_basic>
@@ -19,7 +23,10 @@ public interface TeacherInformationManagementService {
 			throws Exception;
 
 	/**
-	 * 将教师基础信息List生成bysjglxt_teacher_basic以及基于此生成的bysjglxt_user_teacher，存储到数据库中
+	 * @待测试
+	 * 
+	 * @说明 将教师基础信息List生成bysjglxt_teacher_basic以及基于此生成的bysjglxt_user_teacher，
+	 *     存储到数据库中
 	 * 
 	 * @param teacherBasicList
 	 *            教师基础信息List
@@ -28,15 +35,19 @@ public interface TeacherInformationManagementService {
 	public boolean saveTeacherList(List<bysjglxt_teacher_basic> teacherBasicList);
 
 	/**
-	 * 获取数据库中所有的bysjglxt_teacher_basic记录以及bysjglxt_user_teacher记录，
-	 * 并封装成TeacherInformationDTO的List返回
+	 * @待测试
+	 * 
+	 * @说明 获取数据库中所有的bysjglxt_teacher_basic记录以及bysjglxt_user_teacher记录，
+	 *     并封装成TeacherInformationDTO的List返回
 	 * 
 	 * @return
 	 */
 	public List<TeacherInformationDTO> list_TeacherInformationDTO_All();
 
 	/**
-	 * 通过领导小组组长手动添加的教师记录，生成用户表及基础信息表，一并存入数据库中
+	 * @待测试
+	 * 
+	 * @说明 通过领导小组组长手动添加的教师记录，生成用户表及基础信息表，一并存入数据库中
 	 * 
 	 * @param student_basic
 	 *            手动输入的教师基础信息
@@ -45,12 +56,37 @@ public interface TeacherInformationManagementService {
 	public boolean save_NewTeacher(bysjglxt_teacher_basic teacher_basic);
 
 	/**
-	 * 通过多选删除选中的教师用户表以及基础信息表，
+	 * @待测试
+	 * 
+	 * @说明 通过多选删除选中的教师用户表以及基础信息表，
 	 * 
 	 * @param useStudentNumList
 	 *            由要删除的教师工号组成的列表
 	 * @return 是否删除成功,1是 0否
 	 */
 	public boolean remove_TeacherList(List<String> useTeacherNumList);
+
+	/**
+	 * @DOTO
+	 * 
+	 * @说明 创建教研室
+	 * @param newSection
+	 * @return
+	 */
+	public boolean Create_Section(bysjglxt_section newSection);
+
+	/**
+	 * 
+	 * @DOTO
+	 * 
+	 * @说明 根据页数以及搜索关键词，查询教师信息，并连同页面信息一起封装进TeacherInformationManagementVO
+	 *     筛选：教研室（名称）、性别（中文）
+	 * @param TeacherInformationManagementVO
+	 *            存有需要查询的当前页pageIndex 以及搜索信息search模糊查询教师姓名，并匹配变色并按照工号排序
+	 * 
+	 * @return 封装好的TeacherInformationManagementVO（类中所有页面信息均要封装）
+	 */
+	public TeacherInformationManagementVO VO_TEACHER_By_PageAndSearch(
+			TeacherInformationManagementVO teacherInformationManagementVO);
 
 }
--- src/com/bysjglxt/service/TopicManagementService.java
+++ src/com/bysjglxt/service/TopicManagementService.java
@@ -3,6 +3,7 @@ package com.bysjglxt.service;
 import java.util.List;
 
 import com.bysjglxt.domain.DO.bysjglxt_topic;
+import com.bysjglxt.domain.VO.TopicManagementVO;
 
 public interface TopicManagementService {
 
@@ -22,19 +23,63 @@ public interface TopicManagementService {
 	public boolean DeleteTopic(List<String> topicID);
 
 	/**
+	 * 
 	 * @说明 采纳课题
 	 * @param topicID
-	 * @return
+	 * @return 1成功 0失败
 	 */
 	public boolean adoptTopic(List<String> topicID);
+
 	/**
-	 * @说明 关闭课题
-	 * 
+	 * @说明 关闭课题，(已采纳的课题关闭后不可选)
+	 * @param topicID
+	 * @return 1成功 0失败
 	 */
-	
 	public boolean closeTopic(List<String> topicID);
-	
-	
-	
+
+	/**
+	 * @DOTO
+	 * 
+	 * @说明 根据页数以及搜索关键词，查询课题信息，并连同页面信息一起封装进TopicManagementVO
+	 * 
+	 * @param topicManagementVO
+	 *            存有需要查询的当前页pageIndex 以及搜索信息search模糊查询课题名，并匹配变色并按照创建时间排序，
+	 * @return 封装好的topicManagementVO
+	 */
+	public TopicManagementVO VO_Topic_By_PageAndSearch(TopicManagementVO topicManagementVO);
+
+	/**
+	 * @DOTO
+	 * 
+	 * @说明 1、判断课题是否可选，若可以则下一步操作，不可以则返回false。
+	 *     2、创建一条bysjglxt_topic_select学生选题表的记录，指导老师和评阅老师为null。
+	 *     3、学生课题bysjglxt_topic所属的记录中，课题已选学生数topic_student_num加1。
+	 * 
+	 * @param studentID
+	 *            选题的学生
+	 * 
+	 * @param topicID
+	 *            所选课题
+	 * 
+	 * @return 1成功 0失败
+	 */
+	public boolean selectTopic(String studentID, String topicID);
+
+	/**
+	 * @DOTO
+	 * 
+	 * @说明 某教师提前开放某题给某些学生： 1、判断该教师是否是该课题的指导老师之一，若可以则下一步操作，不可以则返回false。
+	 *     2、将这个学生列表设为改课题的指定学生，（此后该列表中学生就可以提前选该题）
+	 * 
+	 * @param TeacherID
+	 *            教师ID
+	 * @param topicID
+	 *            课题ID
+	 * @param studentIDList
+	 *            学生ID列表
+	 * 
+	 * @return 1成功 0失败
+	 */
+	public boolean topicSelectStudent(String TeacherID, String topicID, List<String> studentIDList);
 
 }
