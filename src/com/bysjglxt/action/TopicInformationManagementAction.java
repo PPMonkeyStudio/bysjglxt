c76bced013d0836a9cb18dcfe762f75f48145d3e
ZB <www.1037976812@qq.com>
2017-11-03 14:49:53 +0800

    修改登录Bug

--- src/com/bysjglxt/action/TopicInformationManagementAction.java
+++ src/com/bysjglxt/action/TopicInformationManagementAction.java
@@ -15,6 +15,7 @@ import com.bysjglxt.domain.VO.TopicInformationManagementVO;
 import com.bysjglxt.service.TopicInformationManagementService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
@@ -28,12 +29,50 @@ public class TopicInformationManagementAction extends ActionSupport
 	private HttpServletResponse http_response;
 
 	private HttpServletRequest http_request;
+
 	/*
 	 * 
 	 */
 	private TopicInformationManagementDTO topicInformationManagementDTO;
 
 	private TopicInformationManagementVO topicInformationManagementVO;
+
+	/**
+	 * 创建新的课题
+	 * 
+	 * @throws IOException
+	 */
+	public void CreateTopic() throws IOException {
+
+		topicInformationManagementDTO.setTeacherInformationDTO(new TeacherInformationDTO());
+		topicInformationManagementDTO.getTeacherInformationDTO().setBysjglxtTeacherUser(new bysjglxt_teacher_user());
+
+		if (ActionContext.getContext().getSession().get("userTeacherDTO") != null) {
+			TeacherInformationDTO userTeacherDTO = (TeacherInformationDTO) ActionContext.getContext().getSession()
+					.get("userTeacherDTO");
+			topicInformationManagementDTO.getTeacherInformationDTO().getBysjglxtTeacherUser()
+					.setUser_teacher_id(userTeacherDTO.getBysjglxtTeacherUser().getUser_teacher_id());
+		} else {
+			http_response.setContentType("text/html;charset=utf-8");
+			http_response.getWriter().write("登录状态已失效");
+			System.out.println("登录状态已失效");
+			return;
+		}
+
+		topicInformationManagementService.CreateTopic(topicInformationManagementDTO);
+		http_response.setContentType("text/html;charset=utf-8");
+		http_response.getWriter().write("success");
+
+	}
+
+	public void ListTopicByPageAndSearch() throws IOException {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.setPrettyPrinting();// 格式化json数据
+		Gson gson = gsonBuilder.create();
+		http_response.setContentType("text/html;charset=utf-8");
+		http_response.getWriter().write(
+				gson.toJson(topicInformationManagementService.VO_Topic_By_PageAndSearch(topicInformationManagementVO)));
+	}
 
 	/**
 	 * @说明 跳转列表页
@@ -52,34 +91,6 @@ public class TopicInformationManagementAction extends ActionSupport
 	 */
 	public String CreateTopicPage() {
 		return "CreateTopicPage";
-	}
-
-	/**
-	 * 创建新的课题
-	 * 
-	 * @throws IOException
-	 */
-	public void CreateTopic() throws IOException {
-
-		topicInformationManagementDTO.setTeacherInformationDTO(new TeacherInformationDTO());
-		topicInformationManagementDTO.getTeacherInformationDTO().setBysjglxtTeacherUser(new bysjglxt_teacher_user());
-
-		topicInformationManagementDTO.getTeacherInformationDTO().getBysjglxtTeacherUser()
-				.setUser_teacher_id("27274613-c87f-40a2-a884-0fea646e5470");
-
-		topicInformationManagementService.CreateTopic(topicInformationManagementDTO);
-		http_response.setContentType("text/html;charset=utf-8");
-		http_response.getWriter().write("success");
-
-	}
-
-	public void ListTopicByPageAndSearch() throws IOException {
-		GsonBuilder gsonBuilder = new GsonBuilder();
-		gsonBuilder.setPrettyPrinting();// 格式化json数据
-		Gson gson = gsonBuilder.create();
-		http_response.setContentType("text/html;charset=utf-8");
-		http_response.getWriter().write(
-				gson.toJson(topicInformationManagementService.VO_Topic_By_PageAndSearch(topicInformationManagementVO)));
 	}
 
 	/*
