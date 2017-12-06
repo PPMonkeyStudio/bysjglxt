62a319d7878c400aaca4b82cfe80d3f496003b99
JXX <ystartor@gmail.com>
2017-12-06 21:11:05 +0800

    pull后报错代码修改

--- src/com/bysjglxt/action/TestAction.java
+++ src/com/bysjglxt/action/TestAction.java
@@ -2,7 +2,6 @@ package com.bysjglxt.action;
 
 import java.util.Date;
 
-import com.bysjglxt.domain.DO.test;
 import com.bysjglxt.service.TestService;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -17,29 +16,6 @@ public class TestAction extends ActionSupport {
 		this.testService = testService;
 	}
 
-	private test tt;
 
-	public test getTt() {
-		if (tt == null) {
-			tt = new test();
-		}
-		return tt;
-	}
-
-	public void setTt(test tt) {
-		this.tt = tt;
-	}
-
-	public String test() {
-		System.out.println("fd");
-		return "success";
-	}
-
-	public String add() {
-		System.out.println(tt.getContent() + "\t" + tt.getId());
-		tt.setCreateTime((new Date()).toString());
-		testService.addTest(tt);
-		return "add";
-	}
 
 }
--- src/com/bysjglxt/service/TestService.java
+++ src/com/bysjglxt/service/TestService.java
@@ -1,7 +1,5 @@
 package com.bysjglxt.service;
 
-import com.bysjglxt.domain.DO.test;
 
 public interface TestService {
-	void addTest(test t);
 }
