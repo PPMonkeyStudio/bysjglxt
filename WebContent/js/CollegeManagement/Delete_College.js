970fd0ffb086d3453d958705e8ce23db4754a586
ystartor <ystartor@gmail.com>
2019-04-14 08:43:14 +0800

    提交

--- WebContent/js/CollegeManagement/Delete_College.js
+++ WebContent/js/CollegeManagement/Delete_College.js
@@ -1,9 +1,11 @@
-function delete_comment() {
+function Delete_College() {
+
 	$
 			.confirm({
-				title : '删除评语',
-				content : '此操作将删除所选择的评语',
+				title : '警告！删除学院',
+				content : '此操作将删除所有所选的数据学院',
 				type : 'red',
+				autoClose : '取消|5000',// 自动关闭
 				buttons : {
 					删除 : {
 						btnClass : 'btn-red',
@@ -15,32 +17,27 @@ function delete_comment() {
 								if (xhr.readyState == 4) {
 									if (xhr.status == 200) {
 										if (xhr.responseText == "success") {
-											toastr.success("删除成功");
-											List_Comment_By_College(CollegeComment_json.pageIndex);
-										}
+											toastr.success("全部学院操作成功");
+											List_College();
+										}else if(xhr.responseText == "error"){
+											toastr.success("部分学院存在数据不被允许删除");
+											List_College();	}
 									} else {
 										toastr.error(xhr.status);
 									}
 								}
 							}
-
 							var checkbox_select = document
 									.getElementsByClassName("checkbox_select");
-
+							var ListDeleteStudentNum = null;
 							var formData = new FormData();
-
 							for (var num = 0; num < checkbox_select.length; num++) {
 								if (checkbox_select[num].checked) {
-									formData.append("listCommentId",
+									formData.append("ListDeleteCollegeID",
 											checkbox_select[num].id);
 								}
-
 							}
-
-							xhr
-									.open("POST",
-											"/bysjglxt/graduationProject/GraduationProjectManagement_deleteListComment");
-
+							xhr.open("POST","/bysjglxt/college/CollegeManagement_DeleteCollege");
 							xhr.send(formData);
 						}
 					},
@@ -48,4 +45,5 @@ function delete_comment() {
 					}
 				}
 			});
+
 }
